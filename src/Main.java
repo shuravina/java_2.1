031ad2d546bf08f5cd905e0e9e3049af641d6caf
Galina Shuravina <shuravinags@mail.ru>
2022-06-15 19:00:55 +0800

    initial commot

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 206; // стоймость билета
+        int rub = 20; // рублей за 1 милю
+        int mil = price / rub;
+        System.out.println("Количество миль начисленных за билет: " + mil) ;
+
+
+
+
+
+    }
+}
